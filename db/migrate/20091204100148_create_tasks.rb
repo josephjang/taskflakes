e95fabd45aef353c0119b53e1dacb41d53c89199
Joseph Jang <josephjang@gmail.com>
2010-01-22 12:58:11 +0900

    indented

--- db/migrate/20091204100148_create_tasks.rb
+++ db/migrate/20091204100148_create_tasks.rb
@@ -1,15 +1,15 @@
 class CreateTasks < ActiveRecord::Migration
-  def self.up
-    create_table :tasks do |t|
-      t.string	:title, :null => false
-      t.text	:description
-	  t.date	:start_date
-	  t.date	:finish_date
-      t.timestamps
-    end
-  end
+	def self.up
+		create_table :tasks do |t|
+			t.string	:title, :null => false
+			t.text		:description
+			t.date		:start_date
+			t.date		:finish_date
+			t.timestamps
+		end
+	end
 
-  def self.down
-    drop_table :tasks
-  end
+	def self.down
+		drop_table :tasks
+	end
 end
