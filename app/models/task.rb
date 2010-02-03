f2880d3c6ddb7c0e9eb21a028f518f727a47ece0
Joseph Jang <josephjang@gmail.com>
2010-02-03 22:07:43 +0900

    added task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ActiveRecord::Base
+	validates_presence_of :title
+	validates_numericality_of :duration, :allow_nil => true, :greater_than => 0, :only_integer => true
+	validates_numericality_of :estimated_duration, :allow_nil => true, :greater_than => 0, :only_integer => true
 end
