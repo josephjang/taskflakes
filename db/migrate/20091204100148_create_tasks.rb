6950f52324a1cde8ebf0537658fceea6f492c35e
Joseph Jang <josephjang@gmail.com>
2010-02-03 19:10:12 +0900

    changed 'task' schema

--- db/migrate/20091204100148_create_tasks.rb
+++ db/migrate/20091204100148_create_tasks.rb
@@ -3,8 +3,14 @@ class CreateTasks < ActiveRecord::Migration
 		create_table :tasks do |t|
 			t.string	:title, :null => false
 			t.text		:description
+			t.string	:owner
+			t.string	:project
 			t.date		:start_date
 			t.date		:finish_date
+			t.date		:estimated_start_date
+			t.date		:estimated_finish_date
+			t.integer	:duration
+			t.integer	:estimated_duration
 			t.timestamps
 		end
 	end
