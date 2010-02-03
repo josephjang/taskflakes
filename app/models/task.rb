1afb6b33884e3b262c5d24f0af53dc9bb9c88540
Joseph Jang <josephjang@gmail.com>
2010-02-03 22:21:55 +0900

    added project-task relationship

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ActiveRecord::Base
+
+	belongs_to :project
+
 	validates_presence_of :title
 	validates_numericality_of :duration, :allow_nil => true, :greater_than => 0, :only_integer => true
 	validates_numericality_of :estimated_duration, :allow_nil => true, :greater_than => 0, :only_integer => true
