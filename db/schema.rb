8fe122923675c0782615ad2d408f954253d46809
Joseph Jang <josephjang@gmail.com>
2010-02-03 22:48:12 +0900

    added project relation to task

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100203131014) do
+ActiveRecord::Schema.define(:version => 20100203132808) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20100203131014) do
     t.integer  "estimated_duration"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "project_id"
   end
 
 end
