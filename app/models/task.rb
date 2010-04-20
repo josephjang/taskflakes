1c70cec84b6de7d55cba1ed38defceb0127669f3
neo <neo@xre0009.nhncorp.com>
2010-04-20 17:11:30 +0900

    duration may not be greater than zero

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ActiveRecord::Base
 	belongs_to :project
 
 	validates_presence_of :title
-	validates_numericality_of :duration, :allow_nil => true, :greater_than => 0, :only_integer => true
+	validates_numericality_of :duration, :allow_nil => true, :greater_than_or_equal_to => 0, :only_integer => true
 	validates_numericality_of :estimated_duration, :allow_nil => true, :greater_than => 0, :only_integer => true
 
     # filter by status
